933819b19fd3c705d16f6058e249485c2e7fd71c
dado26516 <dado26516@edu.ucl.dk>
2021-11-02 09:02:01 +0100

    kontaktformular

--- content/js/kontakt.js
+++ content/js/kontakt.js
@@ -1,3 +1,23 @@
-let email = document.getElementById().value;
-let emne = document.getElementById().value;
-let besked = document.getElementById().value;
+let email = document.getElementsByName('email').value;
+let emne = document.getElementsByName('emne').value;
+let besked = document.getElementsByName('besked').value;
+
+function kontaktForm() {
+
+
+    if (email == "") {
+
+        document.getElementsByName('email').style.borderColor = red;
+        alert("Din besked er sendt");
+    }
+
+    else if (emne == "") {
+
+        document.getElementsByName('email').style.borderColor = red;
+    }
+    else {
+        alert("Din besked er sendt");
+    }
+
+}
+
