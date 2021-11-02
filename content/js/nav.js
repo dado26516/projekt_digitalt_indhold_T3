4a9d8ae0c4626f12c2fce68bbb6d9f02ad6305dd
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2021-11-02 11:45:48 +0100

    ændringer i nav baren
    Co-Authored-By: Tanjaronnemose <83805930+Tanjaronnemose@users.noreply.github.com>

---
+++ content/js/nav.js
@@ -0,0 +1,22 @@
+/*
+var nav = document.getElementById("sticky-nav")
+
+
+window.onscroll = function scroll() {
+
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        nav.style.color = "var(--black)";
+        nav.style.backgroundColor = "var(--white)";
+
+      } else {
+        
+        nav.style.color = "var(--white)";
+        nav.style.backgroundColor = "transparent";
+
+      }
+
+*/
+//nav.style.color = "var(--black)"
+//nav.style.backgroundColor = "var(--white)"
+
+}
