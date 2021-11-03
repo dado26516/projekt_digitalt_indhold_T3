ad171ff8b68f8e393277e95163c350e8d0ed303e
dado26516 <dado26516@edu.ucl.dk>
2021-11-03 10:56:29 +0100

    Update kontakt.js med border-farve

--- content/js/kontakt.js
+++ content/js/kontakt.js
@@ -1,35 +1,38 @@
 
 var email = document.getElementById("email");
 var emne = document.getElementById("emne");
+borderColors = ["3px solid red", "2px solid grey"];
 
 
-function borderColorEmail() {
-    email.style.border = "4px solid red";
-}
-
-function borderColorEmne() {
-    emne.style.border = "4px solid red";
-}
 
 function kontaktForm() {
 
     if (email.value.length == 0 && emne.value.length == 0) {
         console.log("email og emne mangler");
-        borderColorEmail();
-        borderColorEmne();
+        email.style.border = borderColors[0];
+        emne.style.border = borderColors[0];
     }
     else if (email.value.length != 0 && emne.value.length == 0) {
         console.log("emne mangler");
-        borderColorEmne();
+        email.style.border = borderColors[1];
+        emne.style.border = borderColors[0];
+
     }
     else if (email.value.length == 0 && emne.value.length != 0) {
         console.log("mail mangler");
-        borderColorEmail();
+        email.style.border = borderColors[0];
+        emne.style.border = borderColors[1];
     }
 
     else if (email.value.length != 0 && emne.value.length != 0) {
-        console.log("beskeden sendt");
+        alert("beskeden sendt");
+        email.style.border = borderColors[1];
+        emne.style.border = borderColors[1];
     }
 
 
+}
+
+for (var i = 0; i < borderColors.length; i++) {
+    console.log(borderColors[i]);
 }
