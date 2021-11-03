7b1a8625b8c9482b41b4b2430b6d40f2008b52f3
KasperPtak <kasperptak2@gmail.com>
2021-11-03 13:21:09 +0100

    java virker ikke danny fix issue

--- content/js/kontakt.js
+++ content/js/kontakt.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-let email = document.getElementsByName('email').value;
-let emne = document.getElementsByName('emne').value;
-let besked = document.getElementsByName('besked').value;
-
-function kontaktForm() {
-
-
-    if (email == "") {
-
-        document.getElementsByName('email').style.borderColor = red;
-        alert("Din besked er sendt");
-    }
-
-    else if (emne == "") {
-
-        document.getElementsByName('email').style.borderColor = red;
-    }
-    else {
-        alert("Din besked er sendt");
-    }
-
-=======
 var email = document.getElementById("email");
 var emne = document.getElementById("emne");
 var right = "left";
@@ -93,6 +70,5 @@ function kontaktForm() {
         emptySubject.style.display = "none";
         emptyMail.style.display = "none";
     }
->>>>>>> Stashed changes
 }
 
