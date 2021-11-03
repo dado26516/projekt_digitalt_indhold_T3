ccdde65105d764988be446074c48872889334d71
dado26516 <dado26516@edu.ucl.dk>
2021-11-03 10:25:08 +0100

    Kontakt.js
    Co-Authored-By: KasperPtak <60694854+KasperPtak@users.noreply.github.com>

--- content/js/kontakt.js
+++ content/js/kontakt.js
@@ -1,23 +1,35 @@
-let email = document.getElementsByName('email').value;
-let emne = document.getElementsByName('emne').value;
-let besked = document.getElementsByName('besked').value;
+
+var email = document.getElementById("email");
+var emne = document.getElementById("emne");
+
+
+function borderColorEmail() {
+    email.style.border = "4px solid red";
+}
+
+function borderColorEmne() {
+    emne.style.border = "4px solid red";
+}
 
 function kontaktForm() {
 
-
-    if (email == "") {
-
-        document.getElementsByName('email').style.borderColor = red;
-        alert("Din besked er sendt");
+    if (email.value.length == 0 && emne.value.length == 0) {
+        console.log("email og emne mangler");
+        borderColorEmail();
+        borderColorEmne();
+    }
+    else if (email.value.length != 0 && emne.value.length == 0) {
+        console.log("emne mangler");
+        borderColorEmne();
+    }
+    else if (email.value.length == 0 && emne.value.length != 0) {
+        console.log("mail mangler");
+        borderColorEmail();
     }
 
-    else if (emne == "") {
+    else if (email.value.length != 0 && emne.value.length != 0) {
+        console.log("beskeden sendt");
+    }
 
-        document.getElementsByName('email').style.borderColor = red;
-    }
-    else {
-        alert("Din besked er sendt");
-    }
 
 }
-
