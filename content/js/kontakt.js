40506f8c7de1029b0291e46228b26228fbb6795f
dado26516 <dado26516@edu.ucl.dk>
2021-11-03 13:08:56 +0100

    kontakt.Js

--- content/js/kontakt.js
+++ content/js/kontakt.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let email = document.getElementsByName('email').value;
 let emne = document.getElementsByName('emne').value;
 let besked = document.getElementsByName('besked').value;
@@ -19,5 +20,79 @@ function kontaktForm() {
         alert("Din besked er sendt");
     }
 
+=======
+var email = document.getElementById("email");
+var emne = document.getElementById("emne");
+var right = "left";
+var borderColors = ["3px solid red", "2px solid grey"];
+var empty = ["Mangler emne", "Mangler Mail"];
+var emptyMail = document.getElementById("wrong-mail");
+var emptySubject = document.getElementById("wrong-mail");
+const textColor = "red";
+
+function kontaktForm() {
+
+    if (email.value.length == 0 && emne.value.length == 0) {
+
+        //tekst mail
+        emptyMail.innerHTML = empty[1];
+        emptyMail.style.textAlign = "left";
+        emptyMail.style.color = textColor;
+
+        //tekst emne
+        emptySubject.innerHTML = empty[0];
+        emptySubject.style.textAlign = right";
+        emptySubject.style.color = textColor;
+
+        //console.log("email og emne mangler");
+
+        //border
+        email.style.border = borderColors[0];
+        emne.style.border = borderColors[0];
+    }
+
+
+    else if (email.value.length != 0 && emne.value.length == 0) {
+        //console.log("emne mangler");
+
+        emptySubject.innerHTML = empty[0];
+        emptySubject.style.textAlign = "left";
+
+
+        //border
+        email.style.border = borderColors[1];
+        emne.style.border = borderColors[0];
+
+        emptySubject.style.display = "block";
+        emptyMail.style.display = "none";
+    }
+
+
+    else if (email.value.length == 0 && emne.value.length != 0) {
+        // console.log("mail mangler");
+        email.style.border = borderColors[0];
+        emne.style.border = borderColors[1];
+
+        emptyMail.innerHTML = empty[1];
+        emptyMail.style.textAlign = "left";
+        emptyMail.style.color = textColor;
+
+
+        emptySubject.style.display = "none";
+        emptyMail.style.display = "block";
+    }
+
+
+    else if (email.value.length != 0 && emne.value.length != 0) {
+        alert("beskeden sendt");
+
+        //border
+        email.style.border = borderColors[1];
+        emne.style.border = borderColors[1];
+
+        emptySubject.style.display = "none";
+        emptyMail.style.display = "none";
+    }
+>>>>>>> Stashed changes
 }
 
